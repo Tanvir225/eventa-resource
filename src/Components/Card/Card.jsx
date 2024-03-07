f41a7ad86fe876eb6056287573b6accdb86cd860
Tanvir225 <tanvirrehman200@gmail.com>
2024-03-07 19:00:47 +0600

    card component initial design added

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,51 @@
+import { Link } from "react-router-dom";
+import { FaLocationArrow } from "react-icons/fa";
+
+const Card = () => {
+  return (
+    <div className=" bg-white my-5 p-5 shadow-md rounded-2xl space-y-5 group">
+      {/* profile image & bg  */}
+      <div className="relative">
+        <img
+          className="w-full h-full rounded-2xl bg-gray-500 group-hover:scale-110 duration-200"
+          src="https://img.freepik.com/free-psd/3d-rendering-royal-room-illustration_23-2150185815.jpg?t=st=1709815614~exp=1709819214~hmac=1510c1c37623a23b434bea0351199a3a51aec9ed3b3ac104673fb2e380b8a973&w=740"
+          alt="card navigate ui"
+        />
+        <img
+          className="w-[80px] h-[80px] group-hover:scale-75 duration-300 absolute -bottom-10 left-1/2 -translate-x-1/2 rounded-full bg-gray-400 border border-white"
+          src="https://source.unsplash.com/100x100/?men"
+          alt="card navigate ui"
+        />
+      </div>
+      {/* profile name & role */}
+      <div className="pt-8 text-center space-y-1">
+        <h1 className="text-xl md:text-2xl">Tolarbagh Convention Hall</h1>
+        <Link className="text-[#FF69B4] font-semibold hover:text-gray-400 text-sm">
+          <p className="flex justify-center items-center gap-2 py-2">
+            <FaLocationArrow></FaLocationArrow>View Live Location
+          </p>
+        </Link>
+      </div>
+      {/* post , followers following  */}
+      <div className="flex flex-wrap px-4 gap-5  md:px-8 justify-between items-center">
+        <div className="text-center">
+          <h5 className="font-medium  text-xl">500 persons</h5>
+          <p className="text-sm  text-[#FF69B4]">Capacity</p>
+        </div>
+        <div className="text-center">
+          <h5 className="font-medium text-xl">60k</h5>
+          <p className="text-sm  text-[#FF69B4]">Hall Cost</p>
+        </div>
+        <div className="text-center">
+          <h5 className="font-medium text-xl">1 day ago</h5>
+          <p className="text-sm  text-[#FF1493]">Last Booking</p>
+        </div>
+        <button className="hover:bg-[#FF1493] hover:scale-95 font-medium hover:text-white w-full py-2 rounded-full hover:shadow-xl   text-[#FF1493] shadow-[0px_0px_10px_#E2DADA] t duration-500">
+          View Profile
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,16 @@
-
+import Card from "../../Components/Card/Card";
 
 const Home = () => {
-    return (
-        <div className="container mx-auto px-5 lg:px-0 xl:px-0">
-            this is home
-          
-        </div>
-    );
+  return (
+    <div className="container mx-auto px-5 lg:px-0 xl:px-0">
+      this is home
+      <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-4 ">
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
